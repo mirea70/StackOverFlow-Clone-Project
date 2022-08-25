6d25eb64efb3d9e744ad8586a5616b74d574698b
jaekwan278 <wprhks536@gmail.com>
2022-08-25 11:36:06 +0900

    setting

---
+++ server/src/main/java/com/preproject/stackOverFlowV1/StackOverFlowV1Application.java
@@ -0,0 +1,13 @@
+package com.preproject.stackOverFlowV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverFlowV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverFlowV1Application.class, args);
+	}
+
+}
