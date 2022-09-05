2130fc40ba3b15fc47ca747429e34f121522831f
TaeHyung KIM <th1122kim@gmail.com>
2022-09-05 09:40:37 +0900

    임시 통합본 pull

--- client/public/login.js
+++ client/public/login.js
@@ -27,10 +27,10 @@ const handleLogin = async () => {
         return;
       } else {
         localStorage.setItem("memberId", data.memberId);
-        // localStorage.setItem("email", data.email);
-        // localStorage.setItem("name", data.name);
-        // localStorage.setItem("img", data.profile_image);
-        location.href = "boardMainLogin.html";
+        localStorage.setItem("email", data.email);
+        localStorage.setItem("name", data.name);
+        localStorage.setItem("img", data.profile_image);
+        location.href = "boardMain.html";
       }
     });
 };
@@ -45,3 +45,25 @@ const handleDetail = () => {
   localStorage.setItem("key", 1);
   location.href = "boardDetail.html";
 };
+const email = localStorage.getItem("email")
+const name = localStorage.getItem("name")
+const img = localStorage.getItem("img")
+const loginButton = document.querySelector("#loginButton")
+const sighupButton = document.querySelector("#sighupButton")
+const membericon = document.querySelector(".membericon")
+const name2 = document.querySelector(".name")
+
+
+if(email) {
+  loginButton.classList.add('hide')
+  sighupButton.classList.add('hide')
+  membericon.classList.remove('hide')
+  membericon.src = "image/logo.png"
+  name2.classList.remove('hide')
+  name2.textContent = "고영석"
+}else {
+  loginButton.classList.remove('hide')
+  sighupButton.classList.remove('hide')
+  membericon.classList.add('hide')
+  name2.classList.add('hide')
+}
